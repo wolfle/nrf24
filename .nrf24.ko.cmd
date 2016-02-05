2f17a98ebd5557c161f1852c759aa9ca5ac9f5c5
Bob Guo <wolfle@softhome.net>
2016-02-05 20:45:52 +0800

    .gitigore modified

--- .nrf24.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/bob/gits/nrf24/nrf24.ko := ld -EL -r  -T /home/bob/gits/linux-sunxi/scripts/module-common.lds --build-id  -o /home/bob/gits/nrf24/nrf24.ko /home/bob/gits/nrf24/nrf24.o /home/bob/gits/nrf24/nrf24.mod.o
--- .tmp_versions/nrf24.mod
+++
@@ -1,2 +0,0 @@
-/home/bob/gits/nrf24/nrf24.ko
-/home/bob/gits/nrf24/nrf24.o
